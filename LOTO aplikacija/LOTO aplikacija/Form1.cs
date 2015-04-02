22f0f91f91e3eaeb85da3a9e3740cc5816c7b616
icrnac <ivona.black4@gmail.com>
2015-04-02 23:53:23 +0200

    Napravljena programska logika aplikacije
    Programska logika aplikacije je definirana klasom Loto. Objekt klase
    loto prima korisničke vrijednosti, koje sprema u listu uplaćenih
    brojeva. Također, generira listu dobitnih brojeva, te uspoređujući dvije
    liste izračunava broj pogodaka

--- LOTO aplikacija/LOTO aplikacija/Form1.cs
+++ LOTO aplikacija/LOTO aplikacija/Form1.cs
@@ -10,11 +10,92 @@ using System.Windows.Forms;
 
 namespace LOTO_aplikacija
 {
-    public partial class Form1 : Form
+
+    class Loto {  
+        public List<int> DobitniBrojevi { get; set; }
+        public List<int> UplaceniBrojevi { get; set; }
+        /// <summary> 
+        /// Konstruktor klase. 
+        /// </summary>
+        public Loto() 
+        { 
+          UplaceniBrojevi = new List<int>(); 
+          DobitniBrojevi = new List<int>(); 
+
+        
+        }
+        public bool UnesiUplaceneBrojeve(List<string> korisnickeVrijednosti)
+        {
+            bool ispravni = false;
+            UplaceniBrojevi.Clear();
+            foreach (string v in korisnickeVrijednosti)
+            {
+                int broj = 0;
+                if (int.TryParse(v, out broj) == true)
+                {
+                    if (broj >= 1 && broj <= 39)
+                    {
+                        if (UplaceniBrojevi.Contains(broj) == false)
+                        {
+                            UplaceniBrojevi.Add(broj);
+                        }
+                    }
+                }
+            }
+            if (UplaceniBrojevi.Count == 7)
+            {
+                ispravni = true;
+            }
+            return ispravni;
+        }
+
+        public void GenerirajDobitnuKombinaciju() 
+        { 
+            DobitniBrojevi.Clear();
+            Random generatorBrojeva = new Random();
+            while (DobitniBrojevi.Count < 7) 
+            { 
+                int broj = generatorBrojeva.Next(1, 39); 
+                if (DobitniBrojevi.Contains(broj) == false) 
+                { 
+                    DobitniBrojevi.Add(broj); 
+                } 
+            } 
+        }
+        public int IzracunajBrojPogodaka() 
+        { 
+            int brojPogodaka = 0;
+            foreach (int broj in UplaceniBrojevi) 
+            { 
+                if (DobitniBrojevi.Contains(broj) == true) 
+                { 
+                    brojPogodaka++; 
+                } 
+            } 
+            return brojPogodaka; 
+        }
+    } 
+    
+    public partial class FrmLoto : Form
     {
-        public Form1()
+        public FrmLoto()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
